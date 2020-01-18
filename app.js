620c82d35b50aca7cccf074cd5aa86c3ba403fac
Joshua Mondragon <joshua.mondragon@dragontek.com>
2020-01-18 11:29:19 -0700

    Change logging to 'short' since dokku adds timestamp

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ logger.token('remote-addr', function (req, res) {
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-app.use(logger('combined'));
+app.use(logger('short'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
