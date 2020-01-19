26d8612fda91f32117182e80f1ffa4a696548ae7
Joshua Mondragon <joshua.mondragon@dragontek.com>
2020-01-19 08:23:55 -0700

    Add google analytics support and title configuration using ENV

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Dragontek Cloud Text-to-Speech' });
+  res.render('index', { title: process.env.GCTTS_TITLE || 'Cloud Text-to-Speech', google_analytics: process.env.GOOGLE_ANALYTICS });
 });
 
 module.exports = router;
